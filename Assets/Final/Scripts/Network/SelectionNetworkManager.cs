a320a26b3497c75883a580c2fdd269c03dd6aa57
Fortox <ori6g1@gmail.com>
2025-09-02 00:41:17 +0300

    test8

--- Assets/Final/Scripts/Network/SelectionNetworkManager.cs
+++ Assets/Final/Scripts/Network/SelectionNetworkManager.cs
@@ -186,7 +186,7 @@ public class SelectionNetworkManager : NetworkBehaviour, INetworkRunnerCallbacks
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     public void PlayerDataRequest_RPC([RpcTarget] PlayerRef targetPlayer)
     {
-        Debug.Log($"Session: has session: {PlayerPrefs.HasKey("GameSession")} {PlayerPrefs.GetString("GameSession")}");
+        Debug.Log($"Session: {PlayerPrefs.GetString("GameSession") != networkRunnerRef.CurrentNetworkRunner.SessionInfo.Name}");
         if (!PlayerPrefs.HasKey("GameSession") && PlayerPrefs.GetString("GameSession") != networkRunnerRef.CurrentNetworkRunner.SessionInfo.Name)
         {
             PlayerPrefs.SetString("GameSession", networkRunnerRef.CurrentNetworkRunner.SessionInfo.Name);

b0cf74e781c38856b4b52e5595f9d16089a955d0
Fortox <ori6g1@gmail.com>
2025-09-02 00:49:07 +0300

    test11

--- Assets/Final/Scripts/Network/SelectionNetworkManager.cs
+++ Assets/Final/Scripts/Network/SelectionNetworkManager.cs
@@ -189,6 +189,7 @@ public class SelectionNetworkManager : NetworkBehaviour, INetworkRunnerCallbacks
         //if (!PlayerPrefs.HasKey("GameSession") && PlayerPrefs.GetString("GameSession") != networkRunnerRef.CurrentNetworkRunner.SessionInfo.Name)
         if (PlayerPrefs.GetString("GameSession") != networkRunnerRef.CurrentNetworkRunner.SessionInfo.Name)
         {
+            Debug.Log("My name is");
             PlayerPrefs.SetString("GameSession", networkRunnerRef.CurrentNetworkRunner.SessionInfo.Name);
             PlayerPrefs.SetString("Name", $"Player{targetPlayer.PlayerId}");
             PlayerPrefs.Save();

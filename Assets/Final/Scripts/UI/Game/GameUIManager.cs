74f72009868a080343892a0a4098427aea58aa1c
TomerTzipel <pporpyu@gmail.com>
2025-09-04 02:43:29 +0300

    UI Fix

--- Assets/Final/Scripts/UI/Game/GameUIManager.cs
+++ Assets/Final/Scripts/UI/Game/GameUIManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class GameUIManager : MonoBehaviour
@@ -7,18 +8,40 @@ public class GameUIManager : MonoBehaviour
 
     [SerializeField] private GameObject ScoreboardPanel;
 
+    [SerializeField] private TMP_Text timerText;
+    [SerializeField] private TMP_Text redScoreText;
+    [SerializeField] private TMP_Text blueScoreText;
+
     [SerializeField] private PlayerScoreHandler[] redTeamScoreHandler;
     [SerializeField] private PlayerScoreHandler[] blueTeamScoreHandler;
 
     private void Awake()
     {
         gameCanvas.SetActive(false);
+        ScoreboardPanel.SetActive(false);
+        UpdateTime(0,0);
+        UpdateScore(Team.Red, 0);
+        UpdateScore(Team.Blue, 0);
     }
 
     public void SetUpGameUI()
     {
         selectionCanvas.SetActive(false);
         gameCanvas.SetActive(true);
+    }
+
+    public void UpdateScoreBoard()
+    {
+
+    }
+    public void UpdateTime(int minutes,int secodns)
+    {
+        timerText.text = minutes.ToString("00") + ":" + secodns.ToString("00");
+    }
+    public void UpdateScore(Team team, int score)
+    {
+        if (team == Team.Red) redScoreText.text = score.ToString("00");
+        if (team == Team.Blue) blueScoreText.text = score.ToString("00");
     }
 
 
